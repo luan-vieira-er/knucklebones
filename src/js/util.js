997e95317b2c17516c0cc5ed3d9f665b8d0a8f1f
Luan <luan.vieira@avmb.com.br>
2022-08-25 13:40:53 -0300

    Login

--- src/js/util.js
+++ src/js/util.js
@@ -1,47 +1,9 @@
 const axios = require('axios')
 
-function getIconTipoItem(tipo){
-    switch (tipo) {
-        case 'fogo':
-            return 'mdi-fire'
-        case 'alimentacao':
-            return 'mdi-silverware-variant'
-        case 'online':
-            return 'mdi-wifi'
-        case 'quadro':
-            return 'mdi-image-frame'
-        case 'oficina':
-            return 'mdi-human-male-board'
-        case 'musica':
-            return 'mdi-music'
-        default:
-            return 'mdi-fire'
-    }
-}
-
 function getRoute(route){
     //return 'http://localhost:3000/'.concat(route)
-    return 'https://app-pm-back.herokuapp.com/'.concat(route)
-}
-
-function getColor(tipo, i){
-    switch(tipo){
-        case 'HEX':
-            return getColorHex(i)
-        case 'DESC':
-            return getColorDesc(i)
-        default:
-            return getColorHex(i)
-    }
-}
-
-function getColorHex(i) {
-    return (i + 1) % 3 == 0 ? '#FFCB05' : (i + 2) % 3 == 0 ? '#EE312F' : '#00AB4E'
-}
-
-function getColorDesc(i) {
-    return (i + 1) % 3 == 0 ? 'yellow' : (i + 2) % 3 == 0 ? 'red' : 'green'
-}    
+    return 'https://k-back.herokuapp.com/'.concat(route)
+} 
 
 function validarSessao(vm) {
     const config = {
@@ -62,4 +24,4 @@ function validarSessao(vm) {
         })
 }
 
-module.exports = { getIconTipoItem, getColor, getRoute, validarSessao }
+module.exports = { getRoute, validarSessao }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,51 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSession from 'vue-session'
+import Axios from 'axios'
 
 import Vuetify from 'vuetify/lib'
 
-//import vuetify from './plugins/vuetify'
+/* var global */
+global.keySession = '_keySession'
+global.makeToast = 'makeToast'
+
+/* INTERCEPTOR */
+
+const responseHandler = response => {  
+  if (response.status === 401) {
+    JSON.parse(sessionStorage['vue-session-key'])[global.keySession] = ''
+  }
+  
+  return response;
+};
+
+const errorHandler = (error) => {  
+  
+  if(sessionStorage['vue-session-key'])JSON.parse(sessionStorage['vue-session-key'])[global.keySession] = ''    
+  
+  return Promise.reject(error);
+};
+
+Axios.interceptors.request.use((config) => {    
+  if(sessionStorage['vue-session-key']){        
+    config.headers =  {
+      'x-access-token': JSON.parse(sessionStorage['vue-session-key'])[global.keySession],
+      'Content-type': 'application/json'
+    }
+    return config
+  }else{    
+    return config
+  }
+})
+
+Axios.interceptors.response.use(
+  (response) => responseHandler(response),
+  (error) => errorHandler(error)
+);
 
 Vue.config.productionTip = false
 
+Vue.use(VueSession)
 Vue.use(Vuetify)
 
 const vuetify = new Vuetify({
